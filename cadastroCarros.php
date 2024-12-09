4066f2021326256a6a7236979ee607a85d8543d9
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2024-12-09 13:47:02 -0300

    removendo a pagina de edicao e exclusao de veiculos/usuarios

--- cadastroCarros.php
+++ cadastroCarros.php
@@ -105,8 +105,6 @@ $result = $conn->query($sql);
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
                             <li><a class="dropdown-item" href="cadastroCarros.php">Cadastrar</a></li>
-                            <li><a class="dropdown-item" href="editaVeiculo.php">Editar</a></li>
-                            <li><a class="dropdown-item" href="removeVeiculo.php">Excluir</a></li>
                             <li><a class="dropdown-item" href="listaVeiculos.php">Listar</a></li>
                         </ul>
                     </li>
@@ -115,8 +113,6 @@ $result = $conn->query($sql);
                             Usuário
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink1">
-                            <li><a class="dropdown-item" href="editaUsuario.php">Editar</a></li>
-                            <li><a class="dropdown-item" href="removeUsuarios.php">Remover</a></li>
                             <li><a class="dropdown-item" href="listaUsuarios.php">Listar</a></li>
                         </ul>
                     </li>
