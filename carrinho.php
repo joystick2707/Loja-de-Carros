0c0c6acace40e72eec3f40d0cd4a3e68d68b5a48
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2024-12-13 14:26:42 -0300

    Create carrinho.php

---
+++ carrinho.php
@@ -0,0 +1,31 @@
+<?php
+include "conexao.php";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nome = isset($_POST['nome']) ? $_POST['nome'] : null;
+    $preco = isset($_POST['preco']) ? $_POST['preco'] : null;
+
+    if ($nome && $preco ) {
+        $query_compra = "INSERT INTO carrinho (nome, preco) VALUES (?, ?)";
+        $stmt = $conn->prepare($query_compra);
+
+        if ($stmt) {
+            $stmt->bind_param("sd", $nome, $preco);
+
+            if ($stmt->execute()) {
+                echo "Carro adicionado ao carrinho com sucesso!";
+                header("Location: paginaCompra.php");
+                exit();
+            } else {
+                echo "Erro ao adicionar o carro ao carrinho: " . $stmt->error;
+            }
+        } else {
+            echo "Erro ao preparar a consulta: " . $conn->error;
+        }
+    } else {
+        echo "Dados insuficientes para adicionar ao carrinho.";
+    }
+} else {
+    echo "Método inválido. Use POST para adicionar ao carrinho.";
+}
+?>
