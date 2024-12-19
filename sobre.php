92f25214ebc090204a6b6f43f9b1d30bad25a3d8
Bryan Strey <137365683+joystick2707@users.noreply.github.com>
2024-12-19 15:23:06 -0300

    atualização nos textos de cada slide

--- sobre.php
+++ sobre.php
@@ -66,32 +66,32 @@
             <div class="carousel-item active">
                 <img src="img/perfil/6761a78247a9d.png" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
+                    <h5>CARRO 01</h5>
+                    <p>DESCRIÇÃO DO PRIMEIRO CARRO</p>
                 </div>
             </div>
             <div class="carousel-item">
                 <img src="img/perfil/67585bac7a879.jpeg" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    <p>Some representative placeholder content for the second slide.</p>
+                    <h5>CARRO 02</h5>
+                    <p>DESCRIÇÃO DO SEGUNDO CARRO</p>
                 </div>
             </div>
             <div class="carousel-item">
                 <img src="img/perfil/67585b5a6cc07.jpeg" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>Some representative placeholder content for the third slide.</p>
+                    <h5>CARRO 03</h5>
+                    <p>DESCRIÇÃO DO TERCEIRO CARRO</p>
                 </div>
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
+            <span class="visually-hidden">ANTERIOR</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
+            <span class="visually-hidden">PROXIMO</span>
         </button>
     </div>
 </main>
